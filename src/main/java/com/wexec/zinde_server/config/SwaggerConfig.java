431dd22ebd99f7bcacaac4bcd1f0922591157641
civelekemirhan <civelekemirhann@gmail.com>
2026-03-14 11:15:02 +0300

    cors config fixed

--- src/main/java/com/wexec/zinde_server/config/SwaggerConfig.java
+++ src/main/java/com/wexec/zinde_server/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,9 @@ public class SwaggerConfig {
         final String securitySchemeName = "bearerAuth";
 
         return new OpenAPI()
+                .addServersItem(new Server()
+                        .url("https://robust-nourishment-production-2af5.up.railway.app")
+                        .description("Railway Production"))
                 .info(new Info()
                         .title("Zinde API")
                         .description("Zinde sporcu sosyal medyası backend API")
